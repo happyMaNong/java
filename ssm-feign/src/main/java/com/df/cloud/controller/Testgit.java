03d6aae3694b25c5a9ac70df71c6f59e073ae483
"LiWeiBo" <"LiWeiBo"@GHNISXGKDC1YPN0>
2018-11-23 17:11:10 +0800

    54252

--- ssm-feign/src/main/java/com/df/cloud/controller/Testgit.java
+++ ssm-feign/src/main/java/com/df/cloud/controller/Testgit.java
@@ -2,7 +2,7 @@ package com.df.cloud.controller;
 
 public class Testgit {
 
- 
+
 
   private String nameString ;
 //asdadada a
@@ -10,21 +10,35 @@ public class Testgit {
   *id
   */
   private int id;
-  
+
   private String name ;
   private String name11 ;
-  
+
   //12134454
   //121
   //bobo测试
   //abc
 
-  
+
+
    //波波测试01
+
+   //波波测试01
+
 
 //bobo
 
   //tlp
-  
+
+
   //4545
+
+
+//chongtu
+  //你妈比的
+
+//chongtu
+
+  //三大发放
+
 }
