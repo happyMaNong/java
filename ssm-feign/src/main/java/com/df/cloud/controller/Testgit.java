88dd70195b1fb1ff1a5a9f2994b39542ca6c56f5
tianlingpeng <1194833510@qq.com>
2018-11-26 10:40:03 +0800

    Update Testgit.java

--- ssm-feign/src/main/java/com/df/cloud/controller/Testgit.java
+++ ssm-feign/src/main/java/com/df/cloud/controller/Testgit.java
@@ -2,54 +2,14 @@ package com.df.cloud.controller;
 
 public class Testgit {
 
-//1
-
   private String nameString ;
-//asdadada a
-  /**
-  *id
-  */
+
+
   private int id;
 
   private String name ;
   private String name11 ;
 
-  //12134454
-  //121
-  //bobo测试
-  //abc
+  private String age；
 
-
-
-   //波波测试01
-
-   //波波测试01
-
-
-//bobo
-
-  //tlp
-
-
-  //4545
-
-
-//chongtu
-  //你妈比的
-
-//chongtu
-
-  //三大发放
-//asfewrw4erew
-//dsafdsafsd
-
-
- // fgdfag
-  //asfewrw4erew
-
-
-
-  //wocao
-
-  //搞什么
 }
