a70920304ad896950c0a23ebbfdf3c65071d984e
tianlingpeng <1194833510@qq.com>
2018-11-26 14:47:31 +0800

    尔特

--- ssm-feign/src/main/java/com/df/cloud/controller/Testgit.java
+++ ssm-feign/src/main/java/com/df/cloud/controller/Testgit.java
@@ -2,15 +2,14 @@ package com.df.cloud.controller;
 
 public class Testgit {
 
-
-
   private String nameString ;
-  /**
-  *id
-  */
+ 
   private int id;
 
   private String name ;
   private String name11 ;
+
+
+  private String age；
 
 }
