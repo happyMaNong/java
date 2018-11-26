cbd4f09f6f49ecaf728c2f379fb34ad2dbc9bb72
"LiWeiBo" <"LiWeiBo"@GHNISXGKDC1YPN0>
2018-11-26 15:23:30 +0800

    bobotijiao

--- ssm-feign/src/main/java/com/df/cloud/controller/Testgit.java
+++ ssm-feign/src/main/java/com/df/cloud/controller/Testgit.java
@@ -5,28 +5,13 @@ public class Testgit {
 
   private String bobo;
 
-  private String nameString ;
 
   private int id;
 
   private String name ;
-  private String name11 ;
 
 
-  private String age;
+  private int age;
 
-  private int a;
-
-
-  private int c;
-
- private int bo1;
-
-private int b;
-
-private int pp;
-
-
-private int b2;
 
 }
