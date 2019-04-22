cf1c5c8d65da55465d1f6507b4c1ceebf39bc5f8
tianlingpeng <1194833510@qq.com>
2019-04-22 17:19:59 +0800

    elasticsearch学习

--- es-first/src/test/java/com/tlp/ws/EsFirstApplicationTests.java
+++ es-first/src/test/java/com/tlp/ws/EsFirstApplicationTests.java
@@ -21,6 +21,7 @@ import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.aggregation.AggregatedPage;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
+import org.springframework.data.elasticsearch.core.query.UpdateQuery;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
@@ -39,23 +40,23 @@ import static org.elasticsearch.index.query.QueryBuilders.termQuery;
 @SpringBootTest
 public class EsFirstApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
     @Resource
     private ElasticsearchTemplate elasticsearchTemplate;
 
-	@Autowired
+    @Autowired
     private BlogRepository blogRepository;
 
-	@Autowired
+    @Autowired
     private StudentRepository studentRepository;
 
     @Test
     public void find() {
 
-    //使用queryStringQuery完成单字符串全文查询
+        //使用queryStringQuery完成单字符串全文查询
         SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(queryStringQuery("java")).build();
 
         //查询某个字段中模糊包含目标字符串，使用matchQuery
@@ -72,12 +73,12 @@ public class EsFirstApplicationTests {
         //之前的查询中，当我们输入“文件流”时，ES会把分词后所有包含“文件”和“流”的都查询出来，如果我们希望必须是包含了两个字的才能被查询出来，那么我们就需要设置一下Operator。
         SearchQuery searchQuery4 = new NativeSearchQueryBuilder().withQuery(matchQuery("content", "文件流").operator(Operator.AND)).build();
 
-
-
         SortBuilder sortBuilderName = SortBuilders.fieldSort("realName.keyword").order(SortOrder.DESC);
         SortBuilder sortBuilderId = SortBuilders.fieldSort("id").order(SortOrder.ASC);
         //withSort的前后顺序决定排序优先级
-        SearchQuery searchQuery5 = new NativeSearchQueryBuilder().withQuery(boolQuery().should(rangeQuery("status").lt(1)).must(matchQuery("realName", ""))).withSort(sortBuilderName).withSort(sortBuilderId).build();
+        SearchQuery searchQuery5 = new NativeSearchQueryBuilder()
+                .withQuery(boolQuery().should(rangeQuery("status").lt(1)).must(matchQuery("realName", ""))).withSort(sortBuilderName)
+                .withSort(sortBuilderId).build();
 
         List<Blog> blogs = elasticsearchTemplate.queryForList(searchQuery4, Blog.class);
         System.out.println("1111");
@@ -87,7 +88,8 @@ public class EsFirstApplicationTests {
     public void add() {
         Blog blog = new Blog();
         blog.setId(201904161554L);
-        blog.setContent("C语言是一门面向过程、抽象化的通用程序设计语言，广泛应用于底层开发。C语言能以简易的方式编译、处理低级存储器。C语言是仅产生少量的机器语言以及不需要任何运行环境支持便能运行的高效率程序设计语言。尽管C语言提供了许多低级处理的功能，但仍然保持着跨平台的特性，以一个标准规格写出的C语言程序可在包括一些类似嵌入式处理器以及超级计算机等作业平台的许多计算机平台上进行编译");
+        blog.setContent(
+                "C语言是一门面向过程、抽象化的通用程序设计语言，广泛应用于底层开发。C语言能以简易的方式编译、处理低级存储器。C语言是仅产生少量的机器语言以及不需要任何运行环境支持便能运行的高效率程序设计语言。尽管C语言提供了许多低级处理的功能，但仍然保持着跨平台的特性，以一个标准规格写出的C语言程序可在包括一些类似嵌入式处理器以及超级计算机等作业平台的许多计算机平台上进行编译");
         blog.setPostdate(new Date());
         blog.setTitle("C语言基础知识学习");
         blog.setUrl("www.clang.com");
@@ -132,22 +134,23 @@ public class EsFirstApplicationTests {
         student4.setAddress("美国克利夫兰");
         studentRepository.save(student4);
     }
+
     @Test
     public void find1() {
         //查询条件，条件为空查询所有，es中查询默认有分页 pageNum 1 pageSize 10  matchAll
         SearchQuery searchQuery = new NativeSearchQueryBuilder().withQuery(new MatchAllQueryBuilder()).build();
 
         //match
-       SearchQuery searchQuery1 = new NativeSearchQueryBuilder().withQuery(matchQuery("address","美国")).build();
+        SearchQuery searchQuery1 = new NativeSearchQueryBuilder().withQuery(matchQuery("address", "美国")).build();
 
         //multi_match
-        SearchQuery searchQuery2 = new NativeSearchQueryBuilder().withQuery(multiMatchQuery("美国","address","name")).build();
+        SearchQuery searchQuery2 = new NativeSearchQueryBuilder().withQuery(multiMatchQuery("美国", "address", "name")).build();
 
         //range
         SearchQuery searchQuery3 = new NativeSearchQueryBuilder().withQuery(rangeQuery("age").gt(28).lt(35)).build();
 
         //term 精准匹配 ，查询不到数据是因为，address的类型为text，使用中文分词器分词了，而term对查询的字符串不分词，所以查询不到。
-        SearchQuery searchQuery4 = new NativeSearchQueryBuilder().withQuery(termQuery("address","美国旧金山")).build();
+        SearchQuery searchQuery4 = new NativeSearchQueryBuilder().withQuery(termQuery("address", "美国旧金山")).build();
 
         //exist
         SearchQuery searchQuery5 = new NativeSearchQueryBuilder().withQuery(existsQuery("address")).build();
@@ -155,26 +158,43 @@ public class EsFirstApplicationTests {
         SearchQuery searchQuery6 = new NativeSearchQueryBuilder().withQuery(queryStringQuery("美国")).build();
 
         //bool
-        SearchQuery searchQuery7 = new NativeSearchQueryBuilder().withQuery(boolQuery().must(matchQuery("name","杜小帅"))).build();
+        SearchQuery searchQuery7 = new NativeSearchQueryBuilder().withQuery(boolQuery().must(matchQuery("name", "杜小帅"))).build();
 
-        SearchQuery searchQuery8 = new NativeSearchQueryBuilder().withQuery( boolQuery().must(matchQuery("address","美国")).mustNot(matchQuery("age",30))).build();
+        SearchQuery searchQuery8 = new NativeSearchQueryBuilder()
+                .withQuery(boolQuery().must(matchQuery("address", "美国")).mustNot(matchQuery("age", 30))).build();
 
-        SearchQuery searchQuery9 = new NativeSearchQueryBuilder().withQuery( boolQuery().filter(boolQuery().should(matchQuery("address","美国")).should(matchQuery("name","姚明")))).build();
+        SearchQuery searchQuery9 = new NativeSearchQueryBuilder()
+                .withQuery(boolQuery().filter(boolQuery().should(matchQuery("address", "美国")).should(matchQuery("name", "姚明")))).build();
 
         //分页
 
-        NativeSearchQueryBuilder  searchQuery10 =new NativeSearchQueryBuilder().withQuery(new MatchAllQueryBuilder());
-        Pageable pageable = new PageRequest(1,10);
+        NativeSearchQueryBuilder searchQuery10 = new NativeSearchQueryBuilder().withQuery(new MatchAllQueryBuilder());
+        Pageable pageable = new PageRequest(1, 10);
         searchQuery10.withPageable(pageable);
         //query_string 全文搜索，默认搜索全部字段，可以指定字段
-       // List<Student> students = elasticsearchTemplate.queryForList(searchQuery10, Student.class);
+        // List<Student> students = elasticsearchTemplate.queryForList(searchQuery10, Student.class);
         AggregatedPage<Student> students = elasticsearchTemplate.queryForPage(searchQuery10.build(), Student.class);
 
-        students.getContent().forEach(student->{
+        students.getContent().forEach(student -> {
             System.out.println(student.toString());
         });
     }
 
-
+    @Test
+    public void update() {
+        Student student4 = new Student();
+        student4.setId(2019042205L);
+        student4.setName("欧文");
+        student4.setAge(28);
+        student4.setBirthday("1991-02-21");
+        student4.setScore(100.0);
+        student4.setAddress("美国克利夫兰");
+        studentRepository.save(student4);
     }
+
+    @Test
+    public void delete() {
+        studentRepository.deleteById(2019042204L);
+    }
+}
 
